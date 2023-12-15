e2e35c87b3b7455ad278a6d5f519b2af50c700e6
Liao Dengting <540614105@qq.com>
2023-12-15 15:49:21 +0800

    Update IOSDK.cpp

--- src/unitree_guide/unitree_guide/src/interface/IOSDK.cpp
+++ src/unitree_guide/unitree_guide/src/interface/IOSDK.cpp
@@ -147,6 +147,123 @@ void IOSDK::sendRecv(const LowlevelCmd *cmd, LowlevelState *state){
 
 #ifdef ROBOT_TYPE_Go2
 
+#define TOPIC_LOWCMD "rt/lowcmd"
+#define TOPIC_LOWSTATE "rt/lowstate"
+
+
+IOSDK::IOSDK(){
+
+
+    
+}
+
+
+uint32_t crc32_core(uint32_t* ptr, uint32_t len)
+{
+    unsigned int xbit = 0;
+    unsigned int data = 0;
+    unsigned int CRC32 = 0xFFFFFFFF;
+    const unsigned int dwPolynomial = 0x04c11db7;
+
+    for (unsigned int i = 0; i < len; i++)
+    {
+        xbit = 1 << 31;
+        data = ptr[i];
+        for (unsigned int bits = 0; bits < 32; bits++)
+        {
+            if (CRC32 & 0x80000000)
+            {
+                CRC32 <<= 1;
+                CRC32 ^= dwPolynomial;
+            }
+            else
+            {
+                CRC32 <<= 1;
+            }
+
+            if (data & xbit)
+                CRC32 ^= dwPolynomial;
+            xbit >>= 1;
+        }
+    }
+
+    return CRC32;
+}
+
+
+void IOSDK::Init()
+{
+    InitLowCmd();
+
+    /*create publisher*/
+    lowcmd_publisher.reset(new ChannelPublisher<unitree_go::msg::dds_::LowCmd_>(TOPIC_LOWCMD));
+    lowcmd_publisher->InitChannel();
+
+    /*create subscriber*/
+    lowstate_subscriber.reset(new ChannelSubscriber<unitree_go::msg::dds_::LowState_>(TOPIC_LOWSTATE));
+    lowstate_subscriber->InitChannel(std::bind(&Custom::LowStateMessageHandler, this, std::placeholders::_1), 1);
+
+    /*loop publishing thread*/
+    lowCmdWriteThreadPtr = CreateRecurrentThreadEx("writebasiccmd", UT_CPU_ID_NONE, 2000, &Custom::LowCmdWrite, this);
+}
+
+
+void IOSDK::InitLowCmd()
+{
+    //LowCmd 类型中的 head 成员 表示帧头，
+    //此帧头将用于 CRC 校验。head 、levelFlag、gpio 等按例程所示设置为默认值即可。
+    low_cmd.head()[0] = 0xFE;
+    low_cmd.head()[1] = 0xEF;
+    low_cmd.level_flag() = 0xFF;
+    low_cmd.gpio() = 0;
+
+     /*LowCmd 类型中有 20 个 motorCmd 成员，
+    每一个成员的命令用于控制 Go2 机器人上相对应的一个电机，
+    但 Go2 机器人上只有 12 个电机，
+    故仅有前 12 个有效，剩余的8个起保留作用。*/
+    for(int i=0; i<12; i++)
+    {
+        /*此行命令中将 motorCmd 成员的 mode 变量设置为 0x01，
+        0x01 表示将电机设置为伺服模式。
+        如果用户在调试过程中发现无法控制 Go2 机器人的关节电机，
+        请检查变量的值是否为0x01。*/
+        low_cmd.motor_cmd()[i].mode() = (0x01);   // motor switch to servo (PMSM) mode
+        
+        
+        low_cmd.motor_cmd()[i].q() = (PosStopF);
+        low_cmd.motor_cmd()[i].kp() = (0);
+        low_cmd.motor_cmd()[i].dq() = (VelStopF);
+        low_cmd.motor_cmd()[i].kd() = (0);
+        low_cmd.motor_cmd()[i].tau() = (0);
+    }
+}
+
+void IOSDK::LowStateMessageHandler(const void* message)
+{
+    low_state = *(unitree_go::msg::dds_::LowState_*)message;
+}
+
+void IOSDK::sendRecv(const LowlevelCmd *cmd, LowlevelState *state)
+{
+    motiontime++;
+
+    if (motiontime >= 0)
+    {
+        low_cmd.motor_cmd()[2].q() = qDes[2];
+        low_cmd.motor_cmd()[2].dq() = 0;
+        low_cmd.motor_cmd()[2].kp() = Kp[2];
+        low_cmd.motor_cmd()[2].kd() = Kd[2];
+        low_cmd.motor_cmd()[2].tau() = 0;
+    }
+
+    /*此段代码中第一行首先计算了 CRC 校验码。
+    最后一行代码表示调用 lowcmd_publisher的Write()函数将控制命令发送给 Go2 机器人。*/
+    low_cmd.crc() = crc32_core((uint32_t *)&low_cmd, (sizeof(unitree_go::msg::dds_::LowCmd_)>>2)-1);
+    lowcmd_publisher->Write(low_cmd);
+}
+
+
+
 
 
 #endif
